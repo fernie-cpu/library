65e92c84a771476d21e1f77137c11fa88ed49c22
fernie-cpu <yoloisjustanick@gmail.com>
2021-01-27 19:47:09 -0300

    refactored "Book" to use it class

--- script.js
+++ script.js
@@ -23,11 +23,13 @@ window.addEventListener("click", (event) => {
 let myLibrary = [];
 let newBook;
 
-function Book(title, author, pages, read) {
+class Book {
+    constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    }
 }
 
 function addBookToLibrary() {
