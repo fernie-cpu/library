27982a8a86fa8b2db927947945b5b650109a6ca7
fernie-cpu <yoloisjustanick@gmail.com>
2021-03-17 15:53:22 -0300

    implement firebase

--- script.js
+++ script.js
@@ -1,132 +1,198 @@
 //call HTML elements
+const signBtn = document.querySelector('.signIn');
+const signOutBtn = document.querySelector('.signOut');
+const userPic = document.querySelector('.user-pic');
+const userNameEl = document.querySelector('.user-name');
 const modalBtn = document.querySelector('.addBtn');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.closeModal');
 const titleInput = document.querySelector('.title');
 const authorInput = document.querySelector('.author');
 const pagesInput = document.querySelector('.pages');
-const readInput = document.querySelector('.readBtn')
+const readInput = document.querySelector('.readBtn');
 const addBook = document.querySelector('.submitBtn');
 const library = document.querySelector('.shelf');
 
 //event listeners
 modalBtn.addEventListener('click', () => modal.classList.add('modal-active'));
-closeModal.addEventListener('click', () => modal.classList.remove('modal-active'));
+closeModal.addEventListener('click', () =>
+  modal.classList.remove('modal-active')
+);
 addBook.addEventListener('click', addBookToLibrary);
-window.addEventListener("click", (event) => {
-    if(event.target === modal) {
-        modal.classList.remove('modal-active');
-    }
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.classList.remove('modal-active');
+  }
 });
+
+//firebase
+const signIn = () => {
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(googleProvider);
+};
+
+const signOut = () => {
+  firebase.auth().signOut();
+};
+
+const getProfilePicUrl = () => {
+  return firebase.auth().currentUser.photoURL || '/img/profile_placeholder.png';
+};
+
+const getUserName = () => {
+  return firebase.auth().currentUser.displayName;
+};
+
+const isUserSignedIn = () => {
+  return !!firebase.auth().currentUser;
+};
+
+const initFirebaseAuth = () => {
+  firebase.auth().onAuthStateChanged(authStateObserver);
+};
+
+const authStateObserver = (user) => {
+  if (user) {
+    let profilePicUrl = getProfilePicUrl();
+    let userName = getUserName();
+
+    userPic.style.backgroundImage =
+      'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+    userNameEl.textContent = userName;
+
+    userNameEl.removeAttribute('hidden');
+    userPic.removeAttribute('hidden');
+    signOutBtn.removeAttribute('hidden');
+
+    signBtn.setAttribute('hidden', 'true');
+  } else {
+    userNameEl.setAttribute('hidden', 'true');
+    userPic.setAttribute('hidden', 'true');
+    signOutBtn.setAttribute('hidden', 'true');
+
+    signBtn.removeAttribute('hidden');
+  }
+};
+
+const addSizeToGoogleProfilePic = (url) => {
+  if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
+    return url + '?sz=150';
+  }
+  return url;
+};
+
+initFirebaseAuth();
+
+const dbLibraryRef = firebase.database().ref('myLibrary');
+
+signBtn.addEventListener('click', signIn);
+signOutBtn.addEventListener('click', signOut);
 
 //create book and stored them in an empty array
 let myLibrary = [];
 let newBook;
 
 class Book {
-    constructor(title, author, pages, read) {
+  constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    }
+    this.read = read.checked;
+  }
 }
 
 function addBookToLibrary() {
-    let title = titleInput.value;
-    let author = authorInput.value;
-    let pages = pagesInput.value;
-    let read = readInput.value;
-    newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-    pushLocalStorage();
-    render();
-    modal.style.display = 'none';
+  let title = titleInput.value;
+  let author = authorInput.value;
+  let pages = pagesInput.value;
+  let read = readInput.value;
+  newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  dbLibraryRef.push(newBook);
+  pushLocalStorage();
+  render();
+  modal.style.display = 'none';
 }
 
 //display book
 function render() {
-    const library = document.querySelector(".shelf");
-    const books = document.querySelectorAll(".book");
-    books.forEach((book) => library.removeChild(book));
-    for (let i = 0; i < myLibrary.length; i++) {
-      bookElements(myLibrary[i]);
-    }
+  const library = document.querySelector('.shelf');
+  const books = document.querySelectorAll('.book');
+  books.forEach((book) => library.removeChild(book));
+  for (let i = 0; i < myLibrary.length; i++) {
+    bookElements(myLibrary[i]);
   }
+}
 
 //create html elements to display
 function bookElements(book) {
-    const library = document.querySelector('.shelf');
-    const bookDiv = document.createElement('div');
-    const titleBook = document.createElement('div');
-    const authorBook = document.createElement('div');
-    const pagesBook = document.createElement('div');
-    const readBtn = document.createElement('button');
-    const deleteBtn = document.createElement('button');
+  const library = document.querySelector('.shelf');
+  const bookDiv = document.createElement('div');
+  const titleBook = document.createElement('div');
+  const authorBook = document.createElement('div');
+  const pagesBook = document.createElement('div');
+  const readBtn = document.createElement('button');
+  const deleteBtn = document.createElement('button');
 
-    bookDiv.classList.add('book');
-    bookDiv.setAttribute('id', myLibrary.indexOf(book));
-    if (book.read === 'yes') {
-        readBtn.textContent = 'I\'ve read';
-        readBtn.classList.add('yes');
-    } else {
-        readBtn.textContent = 'Nope, haven\'t read it yet';
-        readBtn.classList.add('no');
-    }
+  bookDiv.classList.add('book');
+  bookDiv.setAttribute('id', myLibrary.indexOf(book));
+  if (book.read === true) {
+    readBtn.textContent = "I've read";
+    readBtn.classList.add('yes');
+  } else {
+    readBtn.textContent = "Nope, haven't read it yet";
+    readBtn.classList.add('no');
+  }
 
-    titleBook.textContent = 'Title: ' + book.title;
-    titleBook.classList.add('titleBook');
+  titleBook.textContent = 'Title: ' + book.title;
+  titleBook.classList.add('titleBook');
 
-    authorBook.textContent = 'Author: ' + book.author;
-    authorBook.classList.add('authorBook')
-    
-    pagesBook.textContent = book.pages + ' pages';
-    pagesBook.classList.add('pagesBook');
-    
-    deleteBtn.textContent = 'Delete';
+  authorBook.textContent = 'Author: ' + book.author;
+  authorBook.classList.add('authorBook');
 
-    bookDiv.appendChild(titleBook);
-    bookDiv.appendChild(authorBook);
-    bookDiv.appendChild(pagesBook);
-    bookDiv.appendChild(readBtn);
-    bookDiv.appendChild(deleteBtn);
-    library.appendChild(bookDiv);
+  pagesBook.textContent = book.pages + ' pages';
+  pagesBook.classList.add('pagesBook');
 
-    //button to let user change book status to read
-    readBtn.addEventListener('click', (e) => {
-        if (e.target.className === 'yes') {
-            e.target.className = 'no';
-            book.read = 'mo';
-        } else {
-            e.target.className = 'yes';
-            book.read = 'yes'
-        }
-        pushLocalStorage();
-        render();
-    });
+  deleteBtn.textContent = 'Delete';
 
-    //button to delete book
-    deleteBtn.addEventListener('click', () => {
-        myLibrary.splice(myLibrary.indexOf(book), 1);
-        pushLocalStorage();
-        render();
-    });
+  bookDiv.appendChild(titleBook);
+  bookDiv.appendChild(authorBook);
+  bookDiv.appendChild(pagesBook);
+  bookDiv.appendChild(readBtn);
+  bookDiv.appendChild(deleteBtn);
+  library.appendChild(bookDiv);
+
+  //button to let user change book status to read
+  readBtn.addEventListener('click', () => {
+    book.read = !book.read;
+    pushLocalStorage();
+    render();
+  });
+
+  //button to delete book
+  deleteBtn.addEventListener('click', () => {
+    myLibrary.splice(myLibrary.indexOf(book), 1);
+    pushLocalStorage();
+    render();
+  });
 }
 
 //stored library in local storage
 function pushLocalStorage() {
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
 //pull library from local storage when page is refreshed
 function pullLocalStorage() {
-    if (!localStorage.myLibrary) {
-        render();
-    } else {
-        let setLibrary = localStorage.getItem('myLibrary');
-        setLibrary = JSON.parse(setLibrary);
-        myLibrary = setLibrary;
-        render();
-    }
+  if (!localStorage.myLibrary) {
+    render();
+  } else {
+    let setLibrary = localStorage.getItem('myLibrary');
+    setLibrary = JSON.parse(setLibrary);
+    myLibrary = setLibrary;
+    render();
+  }
 }
+
 pullLocalStorage();
